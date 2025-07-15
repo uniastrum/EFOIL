e90027592994d8ac224a72c236d45f69b007a581
uniastrum <uniastrum@gmail.com>
2025-07-15 12:52:00 +0700

    Обновление проекта

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+ require('dotenv').config();
 const express = require('express');
 const { Telegraf } = require('telegraf');
 const sqlite3 = require('sqlite3').verbose();
